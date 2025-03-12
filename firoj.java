22c1f33837f036d4676f381e38657d1cba475d5d
Sourodip Nath <sourodip@klizos.com>
2025-03-12 17:05:52 +0530

    firoj java added

---
+++ firoj.java
@@ -0,0 +1,22 @@
+
+public class Firoj {
+    public static void main(String[] args) {
+        System.out.println("Hello from Java!");
+        
+        // Example variables
+        String name = "Firoj";
+        int age = 25;
+        
+        // Print some information
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        
+        // Simple calculation
+        int sum = calculateSum(10, 20);
+        System.out.println("Sum of 10 and 20 is: " + sum);
+    }
+    
+    public static int calculateSum(int a, int b) {
+        return a + b;
+    }
+}
